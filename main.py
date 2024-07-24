c20e3f8aae25493b117f7e1d9b2d91e4211dcdeb
Fenix0cap <vampir.12010@mail.ru>
2024-07-24 18:24:56 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# 1nd program
+print(12 + 89 - 1)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3nd program
+print((1234 % 1000) // 10 + (5678 % 1000) // 10)
+# 4th program
+print(int(42.13) == int((13.42 - int(13.42)) * 100) or int(13.42) == int((42.13 - int(42.13)) * 100))
