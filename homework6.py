afa409e4898f612ac7283f6532119666ad628e41
Fenix0cap <vampir.12010@mail.ru>
2024-07-28 23:24:37 +1200

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,18 @@
+my_dict = {'Anna': 1982, 'Natasha': 1968, 'Vitalii': 1985, 'Veronika': 2020}
+print(my_dict)
+print(my_dict['Natasha'])
+print(my_dict.get('Roma'))
+my_dict['Roksana'] = 2018
+print(my_dict)
+my_dict.update({'Matvei': 2008,
+                'Anton': 1983})
+print(my_dict)
+a = my_dict.pop('Natasha')
+print(my_dict)
+print(a)
+my_set = {1, 2, 3, 1, 2, 3, 'a', 'b', 'c', 'b', 'c'}
+print(my_set)
+my_set.add(5)
+my_set.add('d')
+my_set.remove(2)
+print(my_set)
